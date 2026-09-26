676dc3956925369a2226161506a4284eff8bdb0b
Player-Pro <playeroscar_88@outlook.com>
2026-09-26 08:29:40 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.acsad.school;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        web.loadUrl("file:///android_asset/app.html");
+        setContentView(web);
+    }
+}
